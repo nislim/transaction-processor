070866d437fe5235a9cfe743828b239cd034bfe7
Nick Aerts <aerts.nick@gmail.com>
2021-08-17 17:33:12 +0200

    Adding code for async operations

--- src/account_manager.rs
+++ src/account_manager.rs
@@ -1,6 +1,8 @@
-use std::collections::{BTreeMap, btree_map::Iter};
+use std::{collections::{BTreeMap, btree_map::Iter}, convert::TryFrom};
 
-use crate::{ClientID, LedgerItem, account::Account, error::ProcessorError};
+use tokio::{sync::mpsc::{Sender, channel}, task::JoinHandle};
+
+use crate::{ClientID, LedgerItem, account::Account, error::ProcessorError, tx_amount::TxAmount};
 
 pub struct AccountManager {
     accounts: BTreeMap<ClientID, Account>
@@ -33,4 +35,110 @@ impl AccountManager {
     pub fn iter(&self) -> Iter<ClientID, Account>{
         self.accounts.iter()
     }
+}
+
+#[derive(Debug)]
+enum AccountManagerMessage {
+    Process(LedgerItem),
+    Dump(Sender<(ClientID, TxAmount, TxAmount, TxAmount, bool)>),
+    Stop,
+}
+
+pub struct AccountManagerTask {
+    handle: JoinHandle<()>,
+    sender: Sender<AccountManagerMessage>,
+}
+
+impl AccountManagerTask {
+    pub fn spawn() -> AccountManagerTask {
+        let (sender, mut receiver) = channel(128);
+
+        let handle = tokio::spawn(async move {
+            let mut manager = AccountManager::new();
+            while let Some(msg) = receiver.recv().await {
+                match msg {
+                    AccountManagerMessage::Process(item) => {
+                        if let Err(e) = manager.process(item) {
+                            eprintln!("{:?}", e);
+                        }
+                    },
+                    AccountManagerMessage::Dump(sender) => {
+                        for (client_id, account) in manager.iter() {
+                            sender.send((*client_id, account.available(), account.held(), account.total(), account.is_locked())).await.unwrap()
+                        }
+                    }
+                    AccountManagerMessage::Stop => {
+                        break;
+                    }
+                }
+            }
+        });
+
+        AccountManagerTask {
+            handle,
+            sender,
+        }
+    }
+
+    pub async fn process(&self, item: LedgerItem) {
+        self.sender.send(AccountManagerMessage::Process(item)).await.unwrap();
+    }
+
+    pub async fn dump(&self, sender: Sender<(ClientID, TxAmount, TxAmount, TxAmount, bool)>) {
+        self.sender.send(AccountManagerMessage::Dump(sender)).await.unwrap()
+    }
+
+    pub async fn stop(&self) {
+        self.sender.send(AccountManagerMessage::Stop).await.unwrap();
+    }
+
+    pub async fn join(self) {
+        self.handle.await.unwrap()
+    }
+}
+
+pub struct AccountManagerLoadbalancer {
+    tasks: Vec<AccountManagerTask>,
+
+    mask: ClientID,
+}
+
+impl AccountManagerLoadbalancer {
+
+    pub fn spawn(count: usize) -> Self {
+        assert!(count.count_ones() == 1, "Number of tasks need to be a power of 2");
+
+        let tasks = (0..count).map(|_| AccountManagerTask::spawn()).collect();
+        let mask = ClientID::try_from(count - 1).unwrap();
+
+        AccountManagerLoadbalancer {
+            tasks,
+            mask,
+        }
+    }
+
+    pub async fn process(&self, item: LedgerItem) {
+        let index = item.client_id & self.mask;
+
+        self.tasks[index as usize].process(item).await;
+    }
+
+    pub async fn dump(&self, sender: Sender<(ClientID, TxAmount, TxAmount, TxAmount, bool)>) {
+        for task in self.tasks.iter() {
+            task.dump(sender.clone()).await;
+        }
+    }
+
+    pub async fn stop(&self) {
+        for task in self.tasks.iter() {
+            task.stop().await;
+        }
+    }
+
+    pub async fn join(self) {
+        for task in self.tasks {
+            task.join().await;
+        }
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::{fmt::Display, fs::File, io::{BufRead, BufReader}};
+use std::fmt::Display;
 
-use account_manager::AccountManager;
 use processor::parse_line;
+use tokio::{fs::File, io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter}, sync::mpsc::channel};
 use tx_amount::TxAmount;
+
+use crate::account_manager::AccountManagerLoadbalancer;
 
 mod account;
 mod account_manager;
@@ -35,6 +37,7 @@ impl Display for LedgerAction {
     }
 }
 
+#[derive(Debug)]
 pub struct LedgerItem {
     client_id:  ClientID,
     tx_id:      TxID,
@@ -42,45 +45,67 @@ pub struct LedgerItem {
     action:     LedgerAction,
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let filename = "transactions.csv";
 
-    let input = File::open(filename)?;
+    let input = File::open(filename).await?;
 
     let reader = BufReader::new(input);
-    let mut account_manager = AccountManager::new();
+    let account_manager = AccountManagerLoadbalancer::spawn(8);
 
     let mut index = 0;
 
     let mut lines = reader.lines();
 
     // Skip header
-    lines.next();
+    lines.next_line().await.unwrap();
 
-    for line in lines {
-        if let Ok(line) = line {
-            match parse_line(line.as_str()) {
-                Ok(item) => {
-                    if let Err(e) = account_manager.process(item) {
-                        eprintln!("[Line {}] {:?}", index, e);
-                    }
-                },
-                Err(_) => {
-                    eprintln!("[Line {}] Could not parse line", index);
-                },
-            }
-        } else {
-            eprintln!("Error reading from {}", filename);
-            break;
+    while let Some(line) = lines.next_line().await? {
+        match parse_line(line.as_str()) {
+            Ok(item) => {
+                account_manager.process(item).await
+            },
+            Err(_) => {
+                eprintln!("[Line {}] Could not parse line", index);
+            },
         }
 
         index += 1;
     }
+
+    let (sender, mut receiver) = channel(128);
     
-    println!("client, available, held, total, locked");
-    for (client_id, account) in account_manager.iter() {
-        println!("{}, {}", client_id, account)
+    account_manager.dump(sender).await;
+
+    let stdout = tokio::io::stdout();
+    let mut writer = BufWriter::new(stdout);
+
+    let header = b"client, available, held, total, locked\n";
+    
+    writer.write_all(header).await?;
+    
+    while let Some((client_id, available, held, total, locked)) = receiver.recv().await {
+        writer.write_all(client_id.to_string().as_bytes()).await?;
+        writer.write_all(b", ").await?;
+
+        writer.write_all(available.to_string().as_bytes()).await?;
+        writer.write_all(b", ").await?;
+
+        writer.write_all(held.to_string().as_bytes()).await?;
+        writer.write_all(b", ").await?;
+
+        writer.write_all(total.to_string().as_bytes()).await?;
+        writer.write_all(b", ").await?;
+
+        writer.write_all(locked.to_string().as_bytes()).await?;
+        writer.write_all(b"\n").await?;
     }
+
+    account_manager.stop().await;
+    account_manager.join().await;
+
+    writer.flush().await.unwrap();
 
     Ok(())
 }
